97648a35c6a25ee712c186e74f52f171317f2a09
Antoine HOULBERT <houlbert.antoine@gmail.com>
2020-07-30 14:46:33 +0200

    (feat)Création de la class Classe

---
+++ src/main/java/Classe.java
@@ -0,0 +1,41 @@
+
+public class Classe {
+
+	 private Integer id;
+	 private String nom;
+	 private Integer capacite;
+	 
+	public Classe(Integer id, String nom, Integer capacite) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.capacite = capacite;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getCapacite() {
+		return capacite;
+	}
+
+	public void setCapacite(Integer capacite) {
+		this.capacite = capacite;
+	}
+	 
+	
+	 
+}
