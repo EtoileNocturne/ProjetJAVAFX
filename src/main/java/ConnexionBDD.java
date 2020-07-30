01f4dfe73dd9810a2d9d6e195f59775ae013eb5f
EtoileNocturne <tarik_yildiz@hotmail.fr>
2020-07-30 14:42:25 +0200

    Ajout utilisateur dans ConnexionBDD

--- src/main/java/ConnexionBDD.java
+++ src/main/java/ConnexionBDD.java
@@ -29,6 +29,62 @@ public class ConnexionBDD {
         return maConnexion;
     }
 
+    public void ajouterUtilisateur(Utilisateur uti){
+        String query = "INSERT INTO utilisateur (nom,prenom,telephone," +
+                "adresse,ville,email,mdp,estAdmin) VALUES (?,?,?,?,?,?,?,?);";
+        try {
+            PreparedStatement ps = maConnexion.prepareStatement(query);
+
+            ps.setString(1,uti.getNom());
+            ps.setString(2,uti.getPrenom());
+            ps.setString(3,uti.getTelephone());
+            ps.setString(4,uti.getAdresse());
+            ps.setString(5,uti.getVille());
+            ps.setString(6,uti.getEmail());
+            ps.setString(7,uti.chiffrerMDP(uti.getMdp()));
+            ps.setBoolean(8,uti.isEstAdmin());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String rechercherUtilisateur (String email){
+        String query = "Select mdp from utilisateur where email = ?";
+
+        try {
+            PreparedStatement ps = maConnexion.prepareStatement(query);
+
+            ps.setString(1,email);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                return rs.getString("mdp");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void modifierMdp (String email,String NewMdp){
+        String query = "UPDATE from utilisateur set mdp = ? WHERE email = ?";
+
+        try {
+            PreparedStatement ps = maConnexion.prepareStatement(query);
+
+            ps.setString(1,NewMdp);
+            ps.setString(2,email);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void seDeconnecter(){
         try {
